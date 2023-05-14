df3b342e3cfd0cc69f9158733c3f1e13b078d7af
Naoki Takami <naokitakami@Naokis-MacBook-Pro.local>
2023-05-14 14:45:13 -0700

    Testing Request Validation

--- birdboard/app/Http/Controllers/ProjectsController.php
+++ birdboard/app/Http/Controllers/ProjectsController.php
@@ -12,7 +12,9 @@ class ProjectsController extends Controller {
     }
 
     public function store() {
-        Project::create(request(['title', 'description']));
+        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+
+        Project::create($attributes);
         return redirect('/projects');
     }
 }
