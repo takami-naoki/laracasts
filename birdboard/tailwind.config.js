b11908c8c2d038e67f713a8e227a7bf91f92d3c2
takami-naoki <n120ao@gmail.com>
2023-05-24 10:15:08 -0700

    Go Go Gadget Tailwind

--- birdboard/tailwind.config.js
+++ birdboard/tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    boxShadow: {
+        default: '0 0 5px 0 rgba(0, 0, 0, 0.8)'
+    },
+
+    extend: {
+        colors: {
+            'grey-light': '#F5F6F9',
+            'grey': 'rgba(0, 0, 0, 0.4)'
+        }
+    },
   },
   plugins: [],
 }
