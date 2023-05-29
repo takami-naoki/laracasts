4f9e5c4d44f340709427af6c6cc5108491e59058
takami-naoki <n120ao@gmail.com>
2023-05-29 16:55:08 -0700

    Touch it

--- birdboard/app/Models/Task.php
+++ birdboard/app/Models/Task.php
@@ -10,6 +10,7 @@ class Task extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $touches = ['project'];
 
     public function project() {
         return $this->belongsTo(Project::class);
