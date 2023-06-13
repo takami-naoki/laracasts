2337b6f34a27dcb9d1505b7df5b9e9952fac6261
Naoki Takami <naokitakami@Naokis-MacBook-Pro.local>
2023-06-13 12:22:02 -0700

    Inviting Users as a Feature Test

--- birdboard/routes/web.php
+++ birdboard/routes/web.php
@@ -18,17 +18,12 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => 'auth'], function() {
-//    Route::get('/projects', [\App\Http\Controllers\ProjectsController::class, 'index']);
-//    Route::get('/projects/create', [\App\Http\Controllers\ProjectsController::class, 'create']);
-//    Route::get('/projects/{project}', [\App\Http\Controllers\ProjectsController::class, 'show']);
-//    Route::get('/projects/{project}/edit', [\App\Http\Controllers\ProjectsController::class, 'edit']);
-//    Route::patch('/projects/{project}', [\App\Http\Controllers\ProjectsController::class, 'update']);
-//    Route::post('/projects', [\App\Http\Controllers\ProjectsController::class, 'store']);
-//    Route::delete('/projects/{project}', [\App\Http\Controllers\ProjectsController::class, 'destroy']);
     Route::resource('projects', \App\Http\Controllers\ProjectsController::class);
 
     Route::post('/projects/{project}/tasks', [\App\Http\Controllers\ProjectTasksController::class, 'store'])->name('tasks.store');
     Route::patch('/projects/{project}/tasks/{task}', [\App\Http\Controllers\ProjectTasksController::class, 'update']);
+
+    Route::post('/projects/{project}/invitations', [\App\Http\Controllers\ProjectInvitationsController::class, 'store']);
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
--- birdboard/tests/Feature/InvitaionsTest.php
+++ birdboard/tests/Feature/InvitaionsTest.php
@@ -12,8 +12,41 @@ class InvitaionsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    function a_project_can_invite_a_user() {
-        $this->withoutExceptionHandling();
+    function not_owners_may_not_invite_users() {
+        $this->actingAs(User::factory()->create())
+            ->post( ProjectFactory::create()->path() . '/invitations')
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    function a_project_owner_can_invite_a_user() {
+        $project = ProjectFactory::create();
+        $userToInvite = User::factory()->create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => $userToInvite->email
+            ])
+            ->assertRedirect($project->path());
+
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test */
+    function the_email_address_must_be_associated_with_a_valid_birdboard_account() {
+        $project = ProjectFactory::create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => 'notuser@example.com'
+            ])
+            ->assertSessionHasErrors([
+                'email' => 'The user you are inviting must have a Birdboard account.'
+            ]);
+    }
+
+    /** @test */
+    function invited_uses_may_update_project_details() {
         $project = ProjectFactory::create();
 
         $project->invite($newUser = User::factory()->create());
