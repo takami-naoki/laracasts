13461ecac98adc3ca220e094b25022487d2915e6
Naoki Takami <naokitakami@Naokis-MacBook-Pro.local>
2023-06-21 15:10:42 -0700

    Idea Cards

--- lc-voting/resources/views/index.blade.php
+++ lc-voting/resources/views/index.blade.php
@@ -28,4 +28,56 @@
         </div>
     </div>
 
+
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container bg-white rounded-xl flex hover:shadow-card transition duration-150 ease-in cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in font-bold text-xxs font-bold uppercase rounded-xl px-4 py-3">Vote</button>
+                </div>
+            </div>
+
+            <div class="flex px-2 py-6">
+                <a href="" class="flex-none">
+                    <img src="https://source.unsplash.com/200×200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+                </a>
+
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random title can go here</a>
+                        <div class="text-gray-600 mt-3 line-clamp-3 text-xs">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi commodi cupiditate eos hic nisi perferendis possimus rem sequi. Consectetur, error esse facere iste officia possimus quaerat ratione repellendus soluta?
+                        </div>
+
+                        <div class="flex items-center justify-between mt-6">
+                            <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                                <div>10 hours ago</div>
+                                <div>&bull;</div>
+                                <div>Category 1</div>
+                                <div>&bull;</div>
+                                <div class="text-gray-800">3 Comments</div>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
+                                <button class="bg-gray-100 hover-bg-gray-200 rounded-full h-7 transition ease-in duration-150 py-2 px-3">
+                                    <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
+
+                                    <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                        <li><a href="" class="hover:bg-gray-100 px-5 py-3 transition duration-150 block text-xs">Mark as Spam</a></li>
+                                        <li><a href="" class="hover:bg-gray-100 px-5 py-3 transition duration-150 block text-xs">Delete Post</a></li>
+                                    </ul>
+                                </button>
+                            </div>
+                        </div>
+                    </h4>
+                </div>
+            </div>
+        </div> <!-- end idea-container -->
+
+    </div> <!-- end ideas-container -->
 </x-app-layout>
